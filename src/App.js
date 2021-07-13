ca05395db3a0a80b2a0a05ca7e93d368c6c2a5e5
LindaOjo <linda@buycoins.africa>
2021-07-13 10:48:53 +0100

    improve code

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ function App() {
   //Ref
   const audioRef = useRef(null);
   //State
-  const [songs, setSongs] = useState(data());
+  const [songs] = useState(data());
   const [currentSong, setCurrentSong] = useState(songs[0]);
   const [isPlaying, setIsPlaying] = useState(false);
   const [libraryStatus, setLibraryStatus] = useState(false);
